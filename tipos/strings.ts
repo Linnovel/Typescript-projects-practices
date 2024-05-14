2550bb36690ff2d231caabb71379710910461f48
Linnovel <linnovel_88@hotmail.com>
2024-05-14 16:23:14 -0400

    typescript primitive

---
+++ tipos/strings.ts
@@ -0,0 +1,14 @@
+(() => {
+
+    const batman:string = 'Batman';
+    const linternaVerde:string = "Linterna Verde";
+    const volcanNegro:string = `Volcan Negro`;
+
+// console.log(`yo soy ${batman}`.toLocaleUpperCase());
+ // console.log(batman.toLocaleUpperCase());
+
+
+
+    
+
+})()
