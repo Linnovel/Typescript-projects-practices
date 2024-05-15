1b572febaccf9c94fbae562d4c067a2d556bf485
Linnovel <linnovel_88@hotmail.com>
2024-05-15 17:45:04 -0400

    objetos

---
+++ objetos/objects.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        power: ['Super velocidad', 'Viajar en el tiempo']
+    };
+    flash = {
+        name: 'Superman',
+        age: 60,
+        power: ['super fuerza'],
+        getNombre() {
+            this.name;
+        }
+    };
+})();
---
+++ objetos/objects.ts
@@ -0,0 +1,17 @@
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        power:['Super velocidad', 'Viajar en el tiempo']
+    }
+
+    flash = {
+        name: 'Superman',
+        age: 60,
+        power: ['super fuerza'],
+        getNombre(){
+            this.name
+        }
+    }
+
+})()
